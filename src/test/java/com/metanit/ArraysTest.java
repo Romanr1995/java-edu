d46af731d4d9e560eb6758c08bd9057d5462cda1
Roman Nozdrachev <roman.nozdracheff@yandex.ru>
2021-06-11 22:45:27 +0300

    Добавил дз по тестам

--- src/test/java/com/metanit/ArraysTest.java
+++ src/test/java/com/metanit/ArraysTest.java
@@ -32,10 +32,34 @@ public class ArraysTest {
     }
 
     @Test
+    public void testRotateEmpyArray() {
+        int[] testArr = {};
+        int steps = 1;
+        Arrays.rotate(testArr,steps);
+        assert testArr[0] == 0 && testArr[1] == 0 && testArr[2] == 0 && testArr[3] ==0;
+    }
+
+    @Test
     public void testRotate() {
         int[] testArr = {2,4,7,8};
         int steps = 1;
         Arrays.rotate(testArr,steps);
-        assert testArr = {8,2,4,7};
+        assert testArr[0] == 8 && testArr[1] == 2 && testArr[2] == 4 && testArr[3] == 7;
+    }
+
+    @Test
+    public void testRotateNullArray() {
+        int[] testArr = null;
+        int steps = 1;
+        Arrays.rotate(testArr,steps);
+        assert testArr[0] == 0 && testArr[1] == 0 && testArr[2] == 0 && testArr[3] ==0;
+    }
+
+    @Test
+    public void testRotateNullSteps() {
+        int[] testArr = {2,4,7,8};
+        int steps = 0;
+        Arrays.rotate(testArr,steps);
+        assert testArr[0] == 2 && testArr[1] == 4 && testArr[2] == 7 && testArr[3] == 8;
     }
 }
