19cb1faa709324897c98538e835c0daecfde4e31
Roman Nozdrachev <roman.nozdracheff@yandex.ru>
2021-06-17 22:03:39 +0300

    Rectangle compareTo fixed

--- src/main/java/com/metanit/classes/interfaces/Rectangle.java
+++ src/main/java/com/metanit/classes/interfaces/Rectangle.java
@@ -25,15 +25,13 @@ public class Rectangle implements Comparable<Rectangle> {
             } else if (this.square() < two.square()) {
                 return -1;
             } else {
-                return 0;
-            }
-        } else if (this.square() == two.square() && this.a > 0 && this.b > 0 && two.a > 0 && two.b > 0){
-            if (this.perimetr() > two.perimetr()) {
-                return 1;
-            } else if (this.perimetr() < two.perimetr()) {
-                return -1;
-            } else {
-                return 0;
+                if (this.perimetr() > two.perimetr()) {
+                    return 1;
+                } else if (this.perimetr() < two.perimetr()) {
+                    return -1;
+                } else {
+                    return 0;
+                }
             }
         }
         return 0;
