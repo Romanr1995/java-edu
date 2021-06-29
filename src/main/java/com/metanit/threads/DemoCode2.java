90645a873e248637066a2a831b86e093578aeea9
Roman Nozdrachev <roman.nozdracheff@yandex.ru>
2021-06-29 22:04:49 +0300

    Второе задание по потокам

--- src/main/java/com/metanit/threads/DemoCode2.java
+++ src/main/java/com/metanit/threads/DemoCode2.java
@@ -4,8 +4,18 @@ public class DemoCode2 {
     //нужно выводить сообщение о текущем времени (System.currentTimeMillis()) в 3 разных потоках в течении 10 сек
     //сообщение от каждого потока должно быть в формате T#: <time>, где # - номер потока (0,1,2)
     //частота сообщения от каждого потока - не чаще чем 200мс
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Thread tr1 = new Thread(new MyRunnable());
+        Thread tr2 = new Thread(new MyRunnable());
+        Thread tr3 = new Thread(new MyRunnable());
 
 
+    }
+
+    static class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            System.out.println(System.currentTimeMillis());
+        }
     }
 }
