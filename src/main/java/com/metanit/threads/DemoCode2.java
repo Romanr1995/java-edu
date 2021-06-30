3766e38f1242e027be44183f07485914fd53137a
Roman Nozdrachev <roman.nozdracheff@yandex.ru>
2021-06-30 19:44:01 +0300

    Добавил 2 задания по потокам

--- src/main/java/com/metanit/threads/DemoCode2.java
+++ src/main/java/com/metanit/threads/DemoCode2.java
@@ -5,28 +5,35 @@ public class DemoCode2 {
     //сообщение от каждого потока должно быть в формате T#: <time>, где # - номер потока (0,1,2)
     //частота сообщения от каждого потока - не чаще чем 200мс
     public static void main(String[] args) throws InterruptedException {
-        Thread tr1 = new Thread(new MyRunnable());
-        Thread tr2 = new Thread(new MyRunnable());
-        Thread tr3 = new Thread(new MyRunnable());
+        Thread tr1 = new Thread(new MyRunnable(0));
+        Thread tr2 = new Thread(new MyRunnable(1));
+        Thread tr3 = new Thread(new MyRunnable(2));
 
         tr1.start();
-
+        tr2.start();
+        tr3.start();
 
         tr1.join();
+        tr2.join();
+        tr3.join();
+
 
 
     }
 
     static class MyRunnable implements Runnable {
+       public int number;
+
+        public MyRunnable(int number) {
+            this.number = number;
+        }
+
         @Override
         public void run() {
-            long begin = System.currentTimeMillis();
 
-            System.out.println("begin = " + begin);
-
-            long end = begin + 10000;
-
-            System.out.println("end   = " + end);
+            for (long begin = System.currentTimeMillis(); (begin <= begin+10000); begin += 200) {
+                System.out.println("T" + number + " "+ System.currentTimeMillis());
+            }
         }
     }
 }
--- src/main/java/com/metanit/threads/DemoCode3.java
+++ src/main/java/com/metanit/threads/DemoCode3.java
@@ -1,12 +1,49 @@
 package com.metanit.threads;
 
 public class DemoCode3 {
-    public static void main(String[] args) {
-        DataHolder dataHolder = new DataHolder();
+    static final Object mutex = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        DataHolder dataHolder = new DataHolder(4, 5, 10);
         //нужно создать два потока которые прибавят n раз значение x к dataHolder.data с использованием mutex
+        Thread t1 = new Thread(dataHolder);
+        Thread t2 = new Thread(dataHolder);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("summa = " + dataHolder.summa);
+
     }
 
-    static class DataHolder {
+    static class DataHolder implements Runnable {
         int data = 0;
+        int x;
+        int n;
+        int summa;
+
+        public DataHolder(int data, int x, int n) {
+            this.data = data;
+            this.x = x;
+            this.n = n;
+        }
+
+        public DataHolder(int data) {
+            this.data = data;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < n; i++) {
+                synchronized (mutex) {
+                    summa += x;
+                }
+            }
+        }
     }
 }
