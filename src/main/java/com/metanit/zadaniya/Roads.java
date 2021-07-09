4341c3820dd049e77c5fc41cbdf345429b9dcb6d
Roman Nozdrachev <roman.nozdracheff@yandex.ru>
2021-07-09 17:01:23 +0300

    Добавил задачу про дороги и тесты к ней

--- src/main/java/com/metanit/zadaniya/Roads.java
+++ src/main/java/com/metanit/zadaniya/Roads.java
@@ -2,8 +2,15 @@ package com.metanit.zadaniya;
 
 public class Roads {
 
-    public static int roads(int...n) {
-
-        return 0;
+    public static int roads(int[]...matrix) {
+        int roads = 0;
+        for (int i = 1; i < matrix.length; i++) {
+            for (int k = 0; k < i; k++) {
+                if (matrix[i][k] == 1) {
+                    roads++;
+                }
+            }
+        }
+        return roads;
     }
 }
--- src/test/java/com/metanit/RoadsTest.java
+++ src/test/java/com/metanit/RoadsTest.java
@@ -8,6 +8,34 @@ public class RoadsTest {
 
     @Test
     void матрица5На5() {
-        assertEquals(3,roads(01000,10110,01000,01000,00000));
+        int[] a = {0,1,0,0,0};
+        int[] b = {1,0,1,1,0};
+        int[] c = {0,1,0,0,0};
+        int[] d = {0,1,0,0,0};
+        int[] e = {0,0,0,0,0};
+
+        assertEquals(3,roads(a,b,c,d,e));
+    }
+
+    @Test
+    void матрица5На5Нулевая() {
+        int[] a = {0,0,0,0,0};
+        int[] b = {0,0,0,0,0};
+        int[] c = {0,0,0,0,0};
+        int[] d = {0,0,0,0,0};
+        int[] e = {0,0,0,0,0};
+
+        assertEquals(0,roads(a,b,c,d,e));
+    }
+
+    @Test
+    void матрица4На4С6Ребрами() {
+        int[] a = {1,1,1,1};
+        int[] b = {1,1,1,1};
+        int[] c = {1,1,1,1};
+        int[] d = {1,1,1,1};
+
+
+        assertEquals(6,roads(a,b,c,d));
     }
 }
